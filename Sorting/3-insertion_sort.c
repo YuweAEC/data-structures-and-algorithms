39ca2464222fc36fe6edb7a3af33f64a9ad16ace
Yuvraj Singh <yuwe18b@gmail.com>
2024-02-01 20:49:42 +0530

    refactor: add psuedocode algorithms using comment

--- Sorting/3-insertion_sort.c
+++ Sorting/3-insertion_sort.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 #include <conio.h>
 
-void insertion_sort(int *a, int N)
+void insertion_sort(int *a, int N)  // N is the size of the array
 {
-    int i = 1, j = 0, temp = 0;
-    for (i = 1; i < N; i++)
-    {
-        temp = a[i];
-        for (j = i - 1; j >= 0; j--)
+    int i = 1, j = 0, temp = 0; // i is the index of the element to be inserted, j is the index of the element to be compared with temp, temp is the element to be inserted
+    for (i = 1; i < N; i++)     // i is the index of the element to be inserted
+    {           
+        temp = a[i];  // temp is the element to be inserted
+        for (j = i - 1; j >= 0; j--)   // j is the index of the element to be compared with temp
         {
-            if (a[j] > temp)
+            if (a[j] > temp) // if the element to be compared is greater than the element to be inserted
             {
-                a[j + 1] = a[j];
+                a[j + 1] = a[j]; // shift the element to the right
             }
             else
             {
-                break;
+                break; // if the element to be compared is smaller than the element to be inserted, break the loop
             }
         }
-        a[j + 1] = temp;
+        a[j + 1] = temp; // insert the element
     }
 }
 
 void main()
 {
     int arr[] = {5, 2, 8, 4, 1};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]); // n is the size of the array arr (n = 5) 
     
-    insertion_sort(arr, n);
+    insertion_sort(arr, n); // call the function insertion_sort
 
-    printf("Sorted array: \n");
-    for (int i = 0; i < n; i++)
+    printf("Sorted array: \n"); // print the sorted array
+    for (int i = 0; i < n; i++) // i is the index of the array
     {
-        printf("%d ", arr[i]);
+        printf("%d ", arr[i]); // print the element at index i
     }
 }
